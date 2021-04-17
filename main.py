e8dbf02f7f38d220d4401e0deeb5a8e6e712bfb7
Matt <TheDraca@users.noreply.github.com>
2021-04-17 16:31:26 +0100

    Moved settings into json file

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-#02/04/21
 import os
 import JsonControl
 import AddigyAPI
 
-#Read the api settings from a txt file
-def ReadAPIFile(Option):
-    with open(r"/Volumes/OSX/api.txt", "r") as APISettings:
-        for line in APISettings:
-            if Option in line:
-                Result = line.split("=", 1)[1]
-                return Result.strip()
-
 #Store all our changeable info for later
-ClientID=ReadAPIFile("ClientID")
-ClientSecret=ReadAPIFile("ClientSecret")
-Username=ReadAPIFile("Username")
-Password=ReadAPIFile("Password")
-PolicyID=ReadAPIFile("PolicyID")
+ClientID=JsonControl.GetSetting("ClientID")
+ClientSecret=JsonControl.GetSetting("ClientSecret")
+Username=JsonControl.GetSetting("Username")
+Password=JsonControl.GetSetting("Password")
+PolicyID=JsonControl.GetSetting("PolicyID")
 
 
 #Check if json file exits, if it doesn't build it
@@ -25,17 +16,18 @@ if os.path.exists("MMPR.json") == True:
 else:
     #Create a empty device json file and wait for completion 
     while os.path.exists("MMPR.json") == False:
-        JsonControl.GenJSONFile("MMPR.json")
+        JsonControl.GenJSONFile()
+    print("Please fill out the JSON fill then re-run this script")
 
-    #Populate json with all the devices in the given policy
-    for Device in AddigyAPI.GetAllDevicesInPolicy(ClientID,ClientSecret,PolicyID):
-        JsonControl.SaveFoundDevice("MMPR.json",Device["Device Name"],Device["agentid"])
+#Populate json with all the devices in the given policy
+for Device in AddigyAPI.GetAllDevicesInPolicy(ClientID,ClientSecret,PolicyID):
+    JsonControl.SaveFoundDevice(Device["Device Name"],Device["agentid"])
 
 #Main loop, will run until there are no devices left to do.
-while len(JsonControl.GetDevicesToDo("MMPR.json"))  != 0:
+while len(JsonControl.GetDevicesToDo())  != 0:
     #Compare Online Devices with those in the JSON file, if they are in todo lst then reset the password
     for OnlineDevice in AddigyAPI.GetOnlineDevices(ClientID,ClientSecret):
-        if OnlineDevice["Device Name"] in str(JsonControl.GetDevicesToDo("MMPR.json")):
+        if OnlineDevice["Device Name"] in str(JsonControl.GetDevicesToDo()):
             #Device is online, attempt to reset the password
             print("Device pending: " +OnlineDevice["Device Name"] +" AgentID: " +OnlineDevice["agentid"])
 
@@ -45,7 +37,7 @@ while len(JsonControl.GetDevicesToDo("MMPR.json"))  != 0:
             APIResponse=AddigyAPI.ResetPassword(ClientID,ClientSecret,OnlineDevice["agentid"],Username,HashedPassword)
             if APIResponse == "Success":
                 print("Password changed for: " +OnlineDevice["Device Name"])
-                JsonControl.MarkDeviceAsDone("MMPR.json",OnlineDevice["Device Name"],OnlineDevice["agentid"])
+                JsonControl.MarkDeviceAsDone(OnlineDevice["Device Name"],OnlineDevice["agentid"])
             else:
                 print("Failure for device:" +OnlineDevice["Device Name"])
                 with open("Errors.txt", "a+") as ErrorFile:
