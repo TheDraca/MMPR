12ebbac398725a11b7d9c9f7691e1631d6aa5615
Matt <TheDraca@users.noreply.github.com>
2021-04-17 16:31:07 +0100

    Moved settings into json file

--- JsonControl.py
+++ JsonControl.py
@@ -1,29 +1,39 @@
-#02/04/21
 import json
 
-def GenJSONFile(Filename):
+def GenJSONFile(Filename="MMPR.json"):
     with open(Filename, "w") as JSONFile:
         FileContents={
+            "Settings": {
+                "ClientID" : "",
+                "ClientSecret" : "",
+                "Username" : "LocalAccount",
+                "Password" : "SuperSecretPasswd",
+                "PolicyID" : "TargetPolicyIDHere"
+            },
             "DevicesToDo": {},
             "DevicesDone": {}
             }
         json.dump(FileContents,JSONFile)
 
-def GetCurrentContents(Filename):
+def GetSetting(SettingName,Filename="MMPR.json"):
+    with open(Filename, "r") as JSONFile:
+        return (json.load(JSONFile)["Settings"][SettingName])
+
+def GetCurrentContents(Filename="MMPR.json"):
     with open(Filename, "r") as JSONFile:
         return json.load(JSONFile)
 
-def GetDevicesToDo(Filename):
+def GetDevicesToDo(Filename="MMPR.json"):
     with open(Filename, "r") as JSONFile:
         return (json.load(JSONFile)["DevicesToDo"])
 
-def SaveFoundDevice(Filename, DeviceName, AgentID):
+def SaveFoundDevice(DeviceName, AgentID, Filename="MMPR.json"):
     with open(Filename, "r+") as JSONFile:
         Contents=GetCurrentContents(Filename)
         (Contents["DevicesToDo"])[DeviceName] = [AgentID]
         json.dump(Contents,JSONFile)
 
-def MarkDeviceAsDone(Filename, DeviceName, AgentID):
+def MarkDeviceAsDone(DeviceName, AgentID, Filename="MMPR.json"):
     Contents=GetCurrentContents(Filename)
     with open(Filename, "w+") as JSONFile:
         del Contents["DevicesToDo"][DeviceName]
