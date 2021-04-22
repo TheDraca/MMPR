7b92554dc70c886cc09b604ff7439dbe0959bd2e
Matt <TheDraca@users.noreply.github.com>
2021-04-22 18:53:21 +0100

    Fixed found + done devices not saving correctly

--- JsonControl.py
+++ JsonControl.py
@@ -21,7 +21,7 @@ def GetSetting(SettingName,Filename="MMPR.json"):
 
 def GetCurrentContents(Filename="MMPR.json"):
     with open(Filename, "r") as JSONFile:
-        return json.load(JSONFile)
+        return json.loads(JSONFile.read())
 
 def GetDevicesToDo(Filename="MMPR.json"):
     with open(Filename, "r") as JSONFile:
@@ -32,14 +32,14 @@ def GetDevicesDone(Filename="MMPR.json"):
         return (json.load(JSONFile)["DevicesDone"])
 
 def SaveFoundDevice(DeviceName, AgentID, Filename="MMPR.json"):
-    with open(Filename, "r+") as JSONFile:
-        Contents=GetCurrentContents(Filename)
-        (Contents["DevicesToDo"])[DeviceName] = [AgentID]
+    Contents=GetCurrentContents(Filename)
+    with open(Filename, "w+") as JSONFile:
+        (Contents["DevicesToDo"])[DeviceName] = AgentID
         json.dump(Contents,JSONFile)
 
 def MarkDeviceAsDone(DeviceName, AgentID, Filename="MMPR.json"):
     Contents=GetCurrentContents(Filename)
     with open(Filename, "w+") as JSONFile:
         del Contents["DevicesToDo"][DeviceName]
-        Contents["DevicesDone"][str(DeviceName)] = [str(AgentID)]
+        Contents["DevicesDone"][str(DeviceName)] = str(AgentID)
         json.dump(Contents,JSONFile)
