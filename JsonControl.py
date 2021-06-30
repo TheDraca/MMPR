39a66be2c258d222c0aa95c121c9505132c9ad26
Matt <TheDraca@users.noreply.github.com>
2021-06-30 17:50:32 +0100

    More features, better logging etc
    Password reset requests are now saved with their action IDs so that devices that go missing mid operation won’t hold up the loop
    New log function to print and log status.
    Now check if this is a new attempt or not then generate a default config file
    Sleep between each run, time in seconds is configurable in JSON file as RefreshTime

--- JsonControl.py
+++ JsonControl.py
@@ -8,9 +8,11 @@ def GenJSONFile(Filename="MMPR.json"):
                 "ClientSecret" : "",
                 "Username" : "LocalAccount",
                 "Password" : "SuperSecretPasswd",
-                "PolicyID" : "TargetPolicyIDHere"
+                "PolicyID" : "TargetPolicyIDHere",
+                "RefreshTime": "600"
             },
             "DevicesToDo": {},
+            "DevicesPending": {},
             "DevicesDone": {}
             }
         json.dump(FileContents,JSONFile)
@@ -21,7 +23,7 @@ def GetSetting(SettingName,Filename="MMPR.json"):
 
 def GetCurrentContents(Filename="MMPR.json"):
     with open(Filename, "r") as JSONFile:
-        return json.loads(JSONFile.read())
+        return json.load(JSONFile)
 
 def GetDevicesToDo(Filename="MMPR.json"):
     with open(Filename, "r") as JSONFile:
@@ -31,15 +33,33 @@ def GetDevicesDone(Filename="MMPR.json"):
     with open(Filename, "r") as JSONFile:
         return (json.load(JSONFile)["DevicesDone"])
 
+def GetDevicesPending(Filename="MMPR.json"):
+    with open(Filename, "r") as JSONFile:
+        return (json.load(JSONFile)["DevicesPending"])
+
 def SaveFoundDevice(DeviceName, AgentID, Filename="MMPR.json"):
     Contents=GetCurrentContents(Filename)
     with open(Filename, "w+") as JSONFile:
         (Contents["DevicesToDo"])[DeviceName] = AgentID
         json.dump(Contents,JSONFile)
 
-def MarkDeviceAsDone(DeviceName, AgentID, Filename="MMPR.json"):
+def MarkDeviceAsDone(DeviceName, AgentID, ActionID, Filename="MMPR.json"):
     Contents=GetCurrentContents(Filename)
     with open(Filename, "w+") as JSONFile:
         del Contents["DevicesToDo"][DeviceName]
-        Contents["DevicesDone"][str(DeviceName)] = str(AgentID)
+        del Contents["DevicesPending"][DeviceName]
+        (Contents["DevicesDone"])[DeviceName] = AgentID,ActionID
         json.dump(Contents,JSONFile)
+
+def MarkDeviceAsPending(DeviceName, AgentID, ActionID, Filename="MMPR.json"):
+    Contents=GetCurrentContents(Filename)
+    with open(Filename, "w+") as JSONFile:
+        (Contents["DevicesPending"])[DeviceName] = AgentID,ActionID
+        json.dump(Contents,JSONFile)
+
+def ResetPendingDevice(DeviceName, AgentID, Filename="MMPR.json"):
+    Contents=GetCurrentContents(Filename)
+    with open(Filename, "w+") as JSONFile:
+        del Contents["DevicesPending"][DeviceName]
+        json.dump(Contents,JSONFile)
+    SaveFoundDevice(DeviceName,AgentID)
